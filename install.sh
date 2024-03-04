1ef48abfa4c19a5dbf1b00a2b7d771d1879a32ec
Andrius Kairiukstis <k@c0.lt>
2024-03-04 15:44:05 +0100

    Added support for the nomachine

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 cp ./ru-phonetic-mac-modified.xkb /usr/share/X11/xkb/symbols/
+cp ./ru-phonetic-mac-modified.xkb /usr/NX/share/X11/xkb/symbols/
 cp ./ru-phonetic-mac.evdev.xml /usr/share/X11/xkb/rules/
+cp ./ru-phonetic-mac.evdev.xml /usr/NX/share/X11/xkb/rules/
 udevadm trigger --subsystem-match=input --action=change
